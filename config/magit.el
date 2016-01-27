2c90bfe280e1c6307519b35e75bced4b50ea141e
Viktor Kleen <viktor@kleen.org>
2016-01-26 23:56:35 -0800

    Configure Magit to go fullscreen

---
+++ config/magit.el
@@ -0,0 +1,12 @@
+(global-set-key (kbd "C-x g") 'magit-status)
+
+(defun magit-status--fullscreen (orig-fun &rest args)
+  (window-configuration-to-register :magit-fullscreen)
+  (apply orig-fun args)
+  (delete-other-windows))
+
+(defun magit-quit-window--restore-screen ()
+  (jump-to-register :magit-fullscreen))
+
+(advice-add 'magit-status :around 'magit-status--fullscreen)
+(advice-add 'magit-quit-window :after 'magit-quit-window--restore-screen)
